d042e0ee3458577ac51946b9d9018be65cc7e486
Manjunath G <modimanju2019@gmail.com>
2023-11-06 13:29:01 +0530

    feat: :sparkles: Implemented signIn-Up functionality using Clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
+import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return (
     <main>
-      <h1>Threads</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   )
 }
